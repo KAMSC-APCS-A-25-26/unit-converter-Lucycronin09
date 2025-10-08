bc2c1c1c1fae502b5cbe661885df099e911b5620
Lucycronin09 <lucycronin09@gmail.com>
2025-10-08 14:25:01 +0000

    asd

--- src/main/java/TimeConverter.java
+++ src/main/java/TimeConverter.java
@@ -2,51 +2,7 @@ import java.util.Scanner;
 
 public class TimeConverter {
     public static void main(String[] args) {
-        // TODO: Implement time converter
-        // Requirements:
-        // - Prompt for hours, minutes, and seconds
-        // - Display conversion results and calculation breakdown
-        // - Ask user if they want to convert another time (y/n)
-        // - Loop until user chooses to exit
-
-        /*
-        Convert time to total seconds
-
-Enter hours: 1
-Enter minutes: 28
-Enter seconds: 42
-
-Time Conversion:
-Input: 1 hour, 28 minute, and 42 second
-Total seconds: 5322 seconds
-
-Calculation:
-1 hours × 3600 = 3600 seconds
-28 minutes × 60 = 1680 seconds
-42 seconds = 42 seconds
-Total: 5322 seconds
-
-Do you want to convert another time? (y/n): y
-
-Convert time to total seconds
-
-Enter hours: 2
-Enter minutes: 15
-Enter seconds: 30
-
-Time Conversion:
-Input: 2 hour, 15 minute, and 30 second
-Total seconds: 8130 seconds
-
-Calculation:
-2 hours × 3600 = 7200 seconds
-15 minutes × 60 = 900 seconds
-30 seconds = 30 seconds
-Total: 8130 seconds
-
-Do you want to convert another time? (y/n): n
-Goodbye!
-         */
+  
         Scanner sc = new Scanner(System.in);
         boolean status = true;
         while (status)
@@ -60,9 +16,27 @@ Goodbye!
             System.out.print("Enter seconds: ");
             int timeSeconds = sc.nextInt();
             System.out.println();
-            System.out.println();
             System.out.println("Time Conversion: ");
             System.out.println("Input: " + timeHours + " hour, " + timeMinutes + " minute, and " + timeSeconds + " second.");
+            int totalTime = timeSeconds + (timeHours * 3600) + (timeMinutes * 60);
+            System.out.println("Total seconds: " + totalTime + " seconds");
+            System.out.println();
+            System.out.println("Calculation:");
+            System.out.println(timeHours + " hours × 3600 = " + (timeHours * 3600) + " seconds");
+            System.out.println(timeMinutes + " minutes × 60 = " + (timeMinutes * 60) + " seconds");
+            System.out.println(timeSeconds + " seconds = " + timeSeconds + " seconds");
+            System.out.println("Total: " + totalTime + " seconds");
+            System.out.println("");
+            System.out.println("Do you want to convert another time? (y/n): ");
+            String answer = sc.next();
+            if(answer.equals("n"))
+            {
+                status = false; 
+            }
+            System.out.println("");
+            System.out.println("");
+            System.out.println("");
+        
 
 
         }
